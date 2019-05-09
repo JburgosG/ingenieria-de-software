2149bd01ed77415663ab355370820777831527e9
Ruben Cespedes <rubendcsantis@gmail.com>
2019-05-08 19:04:10 -0500

    Mensajes

---
+++ Codigo/uni2/resources/lang/es/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | @Author Jairo Burgos Guarin
+    |
+    */
+
+    'exists' => ':item ya se encuentra registrado en el sistema, favor verificar.',
+    'created' => ':item se ha creado exitosamente.',
+    'edit' => ':item actualizado exitosamente.',
+    'delete' => ':item eliminado de manera exitosa.'
+
+];
---
+++ Codigo/uni2/resources/lang/es/users.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | @Author Jairo Burgos Guarin
+    |
+    */
+
+    'datatable_name' => 'Nombre',
+    'datatable_email' => 'Correo Electronico',
+    'datatable_age' => 'Edad',
+    'datatable_group' => 'Rol',
+    'datatable_actions' => 'Acciones',
+    'title_create_user' => 'Nuevo Usuario'
+
+];
