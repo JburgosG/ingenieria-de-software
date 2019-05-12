f8b7d75c95a09059e5e05fd9fdbec04575c4291b
Jairo Burgos Guarin <jyburgos40@gmail.com>
2019-05-12 14:20:22 -0500

    Banner Home

--- Codigo/uni2/resources/views/home.blade.php
+++ Codigo/uni2/resources/views/home.blade.php
@@ -2,6 +2,20 @@
 @section('title', 'Inicio')
 @section('content')
 
-    <div class="wrapper wrapper-content animated fadeInRight"></div>
+    <div class="wrapper wrapper-content animated fadeInRight">
+    	<div class="row">
+	        <div class="col-lg-12">
+	            <div class="ibox float-e-margins">
+	                <div class="ibox-content text-center p-md">
+	                    <h2>
+	                        <span class="text-navy">
+	                            <i class="fa fa-star"></i> Bienvenidos a la Universidad Católica de Colombia
+	                        </span> - UNI2
+	                    </h2>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+    </div>
 
 @endsection
