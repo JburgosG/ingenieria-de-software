e16a9016be53d77f31c593dd4c478a6efe277c3f
Jairo Burgos Guarin <jyburgos40@gmail.com>
2019-05-12 14:23:39 -0500

    Evento en Home

--- Codigo/uni2/app/Http/Controllers/HomeController.php
+++ Codigo/uni2/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -22,7 +23,14 @@ class HomeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {        
-        return view('home');
+    {   
+        $where = ['state_id' => 1];        
+        $events = Events::where($where)->orderBy('date', 'desc')->limit(5)->get();
+
+        $data = array(
+            'events' => $events            
+        );
+
+        return view('home', $data);        
     }
 }
--- Codigo/uni2/resources/views/home.blade.php
+++ Codigo/uni2/resources/views/home.blade.php
@@ -15,6 +15,51 @@
 	                </div>
 	            </div>
 	        </div>
+	        <div class="col-lg-5">
+	        	<div class="ibox float-e-margins">
+	                <div class="ibox-title">
+	                    <h5>Últimos eventos</h5>
+	                    <div class="ibox-tools">
+	                        <a class="collapse-link">
+	                            <i class="fa fa-chevron-up"></i>
+	                        </a>
+	                    </div>
+	                </div>
+	                <div class="ibox-content">
+	                    @if(!empty($events) && count($events))
+	                    <table class="table table-bordered">
+	                        <thead>
+	                            <tr>
+	                                <th>#</th>
+	                                <th>Nombre</th>
+	                                <th>Fecha</th>
+	                                <th>Visualizar</th>
+	                            </tr>
+	                        </thead>
+	                        <tbody>
+	                            @foreach($events as $row)
+	                            <tr>
+	                                <td>{{ $row->id }}</td>
+	                                <td>{{ $row->name }}</td>
+	                                <td>{{ date_spanish_full_short($row->date) }}</td>
+	                                <td class="text-center">
+	                                    @include('modules.events.partials.view')
+	                                    <label class="label label-info pointer" data-toggle="modal" data-target="#viewEvent_{{ $row->id }}">
+	                                        <i class="fa fa-eye"></i> Ver
+	                                    </label>
+	                                </td>
+	                            </tr>
+	                            @endforeach
+	                        </tbody>
+	                    </table>
+	                    @else
+	                    <div class="alert alert-warning">
+	                        <i class="fa fa-warning"></i> No existen eventos en el sistema.
+	                    </div>
+	                    @endif
+	                </div>
+            	</div>
+	        </div>
 	    </div>
     </div>
 
