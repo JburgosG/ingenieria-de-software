a681b8c9dfacf3d6af263754135b0aaa2b975320
Steven Arias Chavarro <ariass27@yahoo.com>
2019-05-22 20:35:58 -0500

    Disparar evento importante

--- Codigo/uni2/resources/views/layouts/header.blade.php
+++ Codigo/uni2/resources/views/layouts/header.blade.php
@@ -136,4 +136,29 @@
     {{ csrf_field() }}
 </form>
 
+{{-- Modal Important Event --}}
+@if(!empty(_important()))
+<div class="modal inmodal" id="viewEvent_{{ $important->id }}" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content animated bounceInRight">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">
+                    <span aria-hidden="true">&times;</span>
+                    <span class="sr-only">Cerrar</span>
+                </button>
+                <h4 class="modal-title">{{ $important->name }}</h4>
+                <div class="hr-line-dashed"></div>
+                <img src="{{ url('storage/' . $important->image) }}" class="img-responsive img-thumbnail img-event">
+            </div>
+            <div class="modal-body">
+                <p>{{ $important->description }}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-white" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
+
 @endsection
